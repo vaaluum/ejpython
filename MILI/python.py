ef158bfb9522652e26317abe735dc94030d4a28c
vaaluum <164920526+vaaluum@users.noreply.github.com>
2024-11-08 00:50:51 -0300

    Add files via upload

---
+++ MILI/python.py
@@ -0,0 +1,12 @@
+def sum():
+
+    n1 = int(input("Ingrese el primer numero: "))
+
+
+    n2 = int(input("Ingrese el segundo numero: "))
+
+    resultado = n1 + n2
+
+    print("La suma de los dos digitos es:", resultado)
+
+sum()
